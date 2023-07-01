07d82ea60a9c9e18894daa87efceacec62c7cc04
JunseoMin <minjs4562@gmail.com>
2023-07-01 17:00:40 +0900

    [feat]
    pip plus
    little code

--- model_1st/ModelClass_1.py
+++ model_1st/ModelClass_1.py
@@ -8,9 +8,12 @@ from sklearn.model_selection import train_test_split,KFold
 from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping
 from sklearn.preprocessing import MinMaxScaler 
 from sklearn import preprocessing
+from keras.layers import LSTM
+
 
 # using tree struct
 
+#RNN이니까 n번째 돌때마다 데이터를 노드중 한 노드의 학습 데이터를 많이 수정 - 이 부분 얼만큼 해야할지 정하기
 
 class node_model:
     outputs=0
@@ -20,14 +23,23 @@ class node_model:
         self.data=pd.read_csv(path)
         self.model=model
         
+        
+        
         pass
     
     def prescaler(self):
+
         
-        
+        self.traindata=
+        self.testdata=
         pass
     
     def study(self):
-        pass
+        model=Sequential()
+        model.Add(LSTM(,activation='relu',input_shape=()))
+        
+        
+        
     
+        pass
     
